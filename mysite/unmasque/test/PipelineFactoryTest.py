c8999b6b19fc5424cc378c98e659ca2ae81c7cde
Ahana <sendatahana@gmail.com>
2023-09-14 13:47:02 +0530

    testcase fix

--- mysite/unmasque/test/PipelineFactoryTest.py
+++ mysite/unmasque/test/PipelineFactoryTest.py
@@ -5,18 +5,25 @@ from time import sleep
 from mysite.unmasque.src.pipeline.PipeLineFactory import PipeLineFactory
 from mysite.unmasque.src.pipeline.abstract.generic_pipeline import GenericPipeLine
 
+EXTRACTION_TIME = 10
+NUM_THREADS = 5
+
 
 class MockPipeLine(GenericPipeLine):
     def __init__(self, connectionHelper):
         super().__init__(connectionHelper, "Mock PipeLine")
         self.time_profile = 0
 
-    def extract(self, query):
+    def extract(self, args):
+        query = "test_query_" + str(args[0])
+        bucket = args[1]
         print("Extraction Starts:...")
-        for i in range(10):
+        bucket.append(query)
+        for i in range(EXTRACTION_TIME):
             print(i)
             sleep(1)
         print("... done!")
+        bucket.append(query)
         print("Extracted Query: ", query)
 
 
@@ -26,19 +33,28 @@ class MockPipeLineFactory(PipeLineFactory):
         return MockPipeLine(connectionHelper)
 
 
-def thread_func(name):
+def thread_func(name, bucket):
     print(name)
     factory = MockPipeLineFactory()
-    factory.doJob("test_query_" + str(name), "test")
+    factory.doJob([name, bucket], "test")
     print("bye")
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_rate_limited_to_1(self):
-        for i in range(5):
-            t = threading.Thread(target=thread_func, args=(i,))
+        bucket = []
+        for i in range(NUM_THREADS):
+            t = threading.Thread(target=thread_func, args=(i, bucket,))
             t.start()
+
+        sleep(EXTRACTION_TIME * NUM_THREADS + 1)
+        self.assertEqual(len(bucket), EXTRACTION_TIME)
+        for i in range(0, len(bucket), 2):
+            if bucket[i] != bucket[i + 1]:
+                self.assertTrue(False)
+            else:
+                print(bucket[i])
         self.assertTrue(True)
 
 
