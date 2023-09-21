f9b6eab8a8f3d9dd8296ff65655c9a41ee9f9e7a
Ahana <sendatahana@gmail.com>
2023-09-21 17:03:11 +0530

    signal handler

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -1,5 +1,21 @@
+import signal
+import sys
+
 from mysite.unmasque.src.pipeline.ExtractionPipeLine import ExtractionPipeLine
+from mysite.unmasque.src.pipeline.abstract.TpchSanitizer import TpchSanitizer
 from mysite.unmasque.src.util.ConnectionHelper import ConnectionHelper
+
+
+def signal_handler(signum, frame):
+    print('You pressed Ctrl+C!')
+    sigconn = ConnectionHelper()
+    sigconn.connectUsingParams()
+    sanitizer = TpchSanitizer(sigconn)
+    sanitizer.doJob()
+    sigconn.closeConnection()
+    print("database restored!")
+    sys.exit(0)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -9,6 +25,9 @@ if __name__ == '__main__':
          "group by l_orderkey, o_orderdate, o_shippriority, c_mktsegment limit 4;"
 
     conn = ConnectionHelper()
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
     pipeline = ExtractionPipeLine(conn)
     eq = pipeline.extract(hq)
 
