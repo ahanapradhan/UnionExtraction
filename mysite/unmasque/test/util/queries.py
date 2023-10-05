8d944e0240e76c04e5692b8655b679e7c27596e2
Ahana <sendatahana@gmail.com>
2023-10-05 16:22:07 +0530

    test case change for debugging

--- mysite/unmasque/test/util/queries.py
+++ mysite/unmasque/test/util/queries.py
@@ -44,7 +44,8 @@ Q3 = "select l_orderkey as orderkey, sum(l_discount) as revenue, o_totalprice as
 Q3_1 = "select l_orderkey, sum(l_extendedprice*(1 - l_discount) + l_quantity) as revenue, o_orderdate, " \
        "o_shippriority  from customer, orders, " \
        "lineitem where c_mktsegment = 'BUILDING' and c_custkey = o_custkey and l_orderkey = o_orderkey and o_orderdate " \
-       "< '1995-03-15' and l_shipdate > '1995-03-15' group by l_orderkey, o_orderdate, o_shippriority order by revenue " \
+       "<= '1995-03-15' and l_shipdate >= '1995-03-15' group by l_orderkey, o_orderdate, o_shippriority order by " \
+       "revenue " \
        "desc, o_orderdate limit 10;"
 Q3_2 = "select l_orderkey, sum(l_extendedprice*(1 - l_discount) - o_totalprice) as revenue, o_orderdate, " \
        "o_shippriority  from customer, orders, " \
