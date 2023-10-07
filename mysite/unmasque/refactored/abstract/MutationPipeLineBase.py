d7e44e74530261d5779d2198f320c64b92e96c14
Ahana <sendatahana@gmail.com>
2023-10-07 08:59:26 +0530

    bugfix

--- mysite/unmasque/refactored/abstract/MutationPipeLineBase.py
+++ mysite/unmasque/refactored/abstract/MutationPipeLineBase.py
@@ -1,6 +1,7 @@
 from .ExtractorBase import Base
 from ..executable import Executable
-from ..util.common_queries import get_tabname_4, get_star, truncate_table, create_table_as_select_star_from
+from ..util.common_queries import get_tabname_4, get_star, truncate_table, create_table_as_select_star_from, \
+    insert_into_tab_select_star_fromtab
 
 
 class MutationPipeLineBase(Base):
@@ -31,7 +32,8 @@ class MutationPipeLineBase(Base):
 
     def restore_d_min(self):
         for tab in self.core_relations:
-            self.connectionHelper.execute_sql([truncate_table(tab), create_table_as_select_star_from(tab, get_tabname_4(tab))])
+            self.connectionHelper.execute_sql([truncate_table(tab),
+                                               insert_into_tab_select_star_fromtab(tab, get_tabname_4(tab))])
 
     def extract_params_from_args(self, args):
         return args[0]
