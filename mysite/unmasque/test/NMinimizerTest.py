f3c8b62524fc902e8851654cc342c9ed35cf153e
Ahana <sendatahana@gmail.com>
2023-10-30 20:43:28 +0530

    n minimizer fixed for all sample queries. Reasonable running time

--- mysite/unmasque/test/NMinimizerTest.py
+++ mysite/unmasque/test/NMinimizerTest.py
@@ -80,12 +80,13 @@ class MyTestCase(BaseTestCase):
                 "and n_name = 'ARGENTINA';"
         check = nm.doJob(query)
         self.assertTrue(check)
-        self.assertEqual(1, nm.core_sizes[self.tab_customer])
+        self.assertEqual(2, nm.core_sizes[self.tab_customer])
         self.assertEqual(1, nm.core_sizes[self.tab_orders])
         self.assertEqual(2, nm.core_sizes[self.tab_nation])
         app = Executable(self.conn)
         res = app.doJob(query)
         self.assertTrue(not isQ_result_empty(res))
+        print(res)
         nm.see_d_min()
         self.conn.closeConnection()
 
@@ -111,6 +112,7 @@ class MyTestCase(BaseTestCase):
         # pass
 
     def case2(self):
+        # this testcase takes time due to row by row elim on orders table
         self.conn.connectUsingParams()
         relations = [self.tab_orders, self.tab_customer, self.tab_lineitem]
         nm = NMinimizer(self.conn, relations, tpchSettings.all_size)
@@ -140,47 +142,13 @@ class MyTestCase(BaseTestCase):
                 "SELECT n_name FROM supplier, nation WHERE s_nationkey = n_nationkey AND s_acctbal > 4000;"
         check = nm.doJob(query)
         self.assertTrue(check)
+        nm.see_d_min()
         self.assertEqual(1, nm.core_sizes[self.tab_customer])
         self.assertEqual(1, nm.core_sizes[self.tab_supplier])
-        self.assertEqual(2, nm.core_sizes[self.tab_nation])
+        self.assertEqual(1, nm.core_sizes[self.tab_nation])
         app = Executable(self.conn)
         res = app.doJob(query)
         self.assertTrue(not isQ_result_empty(res))
-        self.conn.closeConnection()
-
-    def test_adonis_case_2_simple_on_orders(self):
-        self.conn.connectUsingParams()
-        relations = [self.tab_orders, self.tab_lineitem]
-        nm = NMinimizer(self.conn, relations, tpchSettings.all_size)
-        nm.mock = True
-        query = "SELECT o_totalprice as price FROM orders , lineitem WHERE o_orderkey = l_orderkey " \
-                "and o_orderdate > DATE '1995-01-01' AND l_shipdate > DATE '1995-01-01';"
-        check = nm.doJob(query)
-        self.assertTrue(check)
-        self.assertEqual(1, nm.core_sizes[self.tab_orders])
-        self.assertEqual(1, nm.core_sizes[self.tab_lineitem])
-        app = Executable(self.conn)
-        res = app.doJob(query)
-        self.assertTrue(not isQ_result_empty(query))
-        self.conn.closeConnection()
-
-    def test_adonis_case_2(self):
-        self.conn.connectUsingParams()
-        relations = [self.tab_orders, self.tab_customer, self.tab_lineitem]
-        nm = NMinimizer(self.conn, relations, tpchSettings.all_size)
-        nm.mock = True
-        query = "SELECT c_acctbal as price FROM customer, orders WHERE c_custkey = o_custkey " \
-                "AND o_orderdate > DATE '1995-01-01' INTERSECT " \
-                "SELECT o_totalprice as price FROM orders, lineitem WHERE o_orderkey = l_orderkey " \
-                "AND l_shipdate > DATE '1995-01-01';"
-        check = nm.doJob(query)
-        self.assertTrue(check)
-        self.assertEqual(1, nm.core_sizes[self.tab_customer])
-        self.assertEqual(1, nm.core_sizes[self.tab_lineitem])
-        self.assertEqual(2, nm.core_sizes[self.tab_orders])
-        app = Executable(self.conn)
-        res = app.doJob(query)
-        self.assertTrue(not isQ_result_empty(query))
         self.conn.closeConnection()
 
 
