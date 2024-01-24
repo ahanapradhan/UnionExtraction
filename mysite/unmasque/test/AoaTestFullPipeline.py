eaf701db5a9af695e7cd0744e592a8dc3c298750
Ahana <sendatahana@gmail.com>
2024-01-24 22:56:22 +0530

    testcase add

--- mysite/unmasque/test/AoaTestFullPipeline.py
+++ mysite/unmasque/test/AoaTestFullPipeline.py
@@ -10,6 +10,20 @@ from mysite.unmasque.test.util.BaseTestCase import BaseTestCase
 
 class MyTestCase(BaseTestCase):
 
+    def test_dormant_aoa(self):
+        self.conn.connectUsingParams()
+        query = "Select l_shipmode, count(*) as count From orders, lineitem " \
+                "Where o_orderkey = l_orderkey and l_commitdate <= l_receiptdate and l_shipdate <= l_commitdate " \
+                "and l_receiptdate >= '1994-01-01' and l_receiptdate <= '1995-01-01' and l_extendedprice <= " \
+                "o_totalprice and l_extendedprice <= 70000 and o_totalprice >= 60000 Group By l_shipmode " \
+                "Order By l_shipmode;"
+        core_rels = ['orders', 'lineitem']
+        aoa, check = self.run_pipeline(core_rels, query)
+        self.assertTrue(check)
+        print(aoa.where_clause)
+
+        self.conn.closeConnection()
+
     def test_multiple_aoa(self):
         self.conn.connectUsingParams()
         core_rels = ['orders', 'lineitem', 'partsupp']
@@ -27,7 +41,31 @@ class MyTestCase(BaseTestCase):
         aoa, check = self.run_pipeline(core_rels, query)
         self.assertTrue(check)
         print(aoa.where_clause)
+        self.assertEqual(aoa.where_clause.count("and"), 8)
+        self.assertEqual(aoa.where_clause.count("<="), 6)
+        self.assertEqual(aoa.where_clause.count("="), 3)
+        self.conn.closeConnection()
 
+    def test_multiple_aoa_1(self):
+        self.conn.connectUsingParams()
+        core_rels = ['orders', 'lineitem', 'partsupp']
+        query = "Select l_quantity, l_shipinstruct From orders, lineitem, partsupp " \
+                "Where ps_partkey = l_partkey " \
+                "and ps_suppkey = l_suppkey " \
+                "and o_orderkey = l_orderkey " \
+                "and l_shipdate >= o_orderdate " \
+                "and ps_availqty <= l_orderkey " \
+                "and l_extendedprice <= 20000 " \
+                "and o_totalprice < 60005 " \
+                "and ps_supplycost < 500 " \
+                "and l_linenumber < 2 " \
+                "Order By l_orderkey Limit 10;"
+        aoa, check = self.run_pipeline(core_rels, query)
+        self.assertTrue(check)
+        print(aoa.where_clause)
+        self.assertEqual(aoa.where_clause.count("and"), 8)
+        self.assertEqual(aoa.where_clause.count("<="), 6)
+        self.assertEqual(aoa.where_clause.count(" ="), 3)
         self.conn.closeConnection()
 
     def test_non_key_aoa_join(self):
