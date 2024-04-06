fa835c72af1802a656aca619ed3edbbf30637618
ahanapradhan <sendatahana@gmail.com>
2024-04-07 00:24:02 +0530

    base testcases pass

--- results/extracted_tech_report_queries/e_Q17.sql
+++ results/extracted_tech_report_queries/e_Q17.sql
@@ -1,4 +1,5 @@
-Could not extract the query due to errors DB MINIMIZATION_DONE.
-Here's what I have as a half-baked answer:
-FROM(q1) = { orders, lineitem, customer, part, region, partsupp, nation, supplier }
-;
+Select Avg(l_extendedprice) as avgtotal
+ From lineitem, part 
+ Where lineitem.l_partkey = part.p_partkey
+ and part.p_brand = 'Brand#52'
+ and part.p_container = 'LG CAN';
--- results/extracted_tech_report_queries/e_Q21.sql
+++ results/extracted_tech_report_queries/e_Q21.sql
@@ -1,10 +1,10 @@
 Select s_name, Count(*) as numwait
  From lineitem, nation, orders, supplier 
- Where l_orderkey = o_orderkey
- and l_suppkey = s_suppkey
- and n_nationkey = s_nationkey
- and o_orderstatus = 'F'
- and n_name = 'GERMANY' 
+ Where lineitem.l_orderkey = orders.o_orderkey
+ and nation.n_nationkey = supplier.s_nationkey
+ and lineitem.l_suppkey = supplier.s_suppkey
+ and orders.o_orderstatus = 'F'
+ and nation.n_name = 'GERMANY' 
  Group By s_name 
  Order By numwait desc, s_name asc 
  Limit 100;
