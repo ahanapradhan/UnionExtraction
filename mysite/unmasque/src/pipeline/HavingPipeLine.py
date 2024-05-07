898d95c898ab870b6c05a31cbe48748d6ffa4832
Alaap Surendran <alaapsurendran@gmail.com>
2024-05-07 14:17:06 +0530

    cleanup: Have the hidden query as a member of the Pipeline class
    Since we are using the pipeline class as the context of execution for
    the current pipeline, it only makes sense to add the hidden query as a
    member of that class.

--- mysite/unmasque/src/pipeline/HavingPipeLine.py
+++ mysite/unmasque/src/pipeline/HavingPipeLine.py
@@ -8,6 +8,7 @@ from ...src.pipeline.abstract.generic_pipeline import GenericPipeLine
 class HavingPipeline(GenericPipeLine, ABC):
     def __init__(self, connectionHelper: AbstractConnectionHelper):
         super().__init__(connectionHelper, "Having PipeLine")
+        self.query: str | None = None
 
     def process(self, query: str):
         return GenericPipeLine.process(self, query)
@@ -15,14 +16,15 @@ class HavingPipeline(GenericPipeLine, ABC):
     def extract(self, query: str):
         # Establish connection to db
         self.connectionHelper.connectUsingParams()
+        self.query = query
 
         # *** - Pipeline operations begin here - ***
 
-        self._from_clause_extraction(query)
+        self._from_clause_extraction()
 
         # *** - Pipeline operations end here   - ***
 
-    def _from_clause_extraction(self, query: str):
+    def _from_clause_extraction(self):
         """
             Helper function to run the From clause extraction routine.
             Does not require a minimized database.
@@ -30,15 +32,17 @@ class HavingPipeline(GenericPipeLine, ABC):
         self.update_state(FROM_CLAUSE + START)
         fc = FromClause(self.connectionHelper)
         self.update_state(FROM_CLAUSE + RUNNING)
-        check = fc.doJob(query)
+        check = fc.doJob(self.query)
         self.update_state(FROM_CLAUSE + DONE)
         self.time_profile.update_for_from_clause(fc.local_elapsed_time, fc.app_calls)
 
         if not check or not fc.done:
             self.logger.error("Some problem while extracting from clause. Aborting!")
             self.core_relations= None
+            self.info[FROM_CLAUSE] = None
             return None, self.time_profile
 
         self.core_relations = fc.core_relations
+        self.info[FROM_CLAUSE] = fc.core_relations
         self.logger.debug(f"Core relations: {self.core_relations}")
 
