1238ee6eb7d285976f718129aca0de2421e5227a
Gopinath Patel <gopinath4301@gmail.com>
2024-05-30 13:01:32 +0530

    Updated

--- mysite/unmasque/test/util/gnp_testqueries.py
+++ mysite/unmasque/test/util/gnp_testqueries.py
@@ -1,45 +1,45 @@
 ###'''FILTER Predicate''' ___ Checking Aggregation too
 #Checking Neg extremes of int and numeric using integers for > operator
-Q1 = '''select p_partkey, p_name, ps_availqty, 
+Q1 = '''select p_partkey, p_name, ps_availqty 
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_partkey > -2147483648 and p_size > -2147483647 and
               ps_suppkey > -2147483646 and p_retailprice > -2147483648 and
-              ps_supplycost > -2147483647'''
+              ps_supplycost > -2147483647;'''
 #Checking Neg extremes of int and numeric using integers for < operator
-Q2 = '''select p_partkey, p_name, ps_availqty, 
+Q2 = '''select p_partkey, p_name, ps_availqty 
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_partkey < 2147483647 and p_size < 2147483646 and
               ps_suppkey < 2147483645 and p_retailprice < 2147483647 and
-              ps_supplycost < 2147483646'''
+              ps_supplycost < 2147483646;'''
 #Checking Neg extremes of int and numeric using integers for >= operator
-Q3 = '''select p_partkey, p_name, ps_availqty, 
+Q3 = '''select p_partkey, p_name, ps_availqty 
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_partkey >= -2147483648 and p_size >= -2147483647 and
               ps_suppkey >= -2147483646 and p_retailprice >= -2147483648 and
-              ps_supplycost >= -2147483647'''
+              ps_supplycost >= -2147483647;'''
 #Checking Neg extremes of int and numeric using integers for <= operator
-Q4 = '''select p_partkey, p_name, ps_availqty, 
+Q4 = '''select p_partkey, p_name, ps_availqty 
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_partkey <= 2147483647 and p_size <= 2147483646 and
               ps_suppkey <= 2147483645 and p_retailprice <= 2147483647 and
-              ps_supplycost <= 2147483646'''
+              ps_supplycost <= 2147483646;'''
 #Checking extremes of numeric and checking between for integers.
-Q5 = '''select p_partkey, p_name, ps_availqty, 
+Q5 = '''select p_partkey, p_name, ps_availqty
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_partkey between -2147483647 and 2147483647 and 
               p_size between -214746 and 354564 and ps_suppkey between 0 and 3211132
-              and p_retailprice <= 2147483647.85 and ps_supplycost <= 2147483646.99'''
+              and p_retailprice <= 2147483647.85 and ps_supplycost <= 2147483646.99;'''
 #Checking between for numerics.
-Q6 = '''select p_partkey, p_name, ps_availqty, 
+Q6 = '''select p_partkey, p_name, ps_availqty 
         from part, partsupp 
         where p_partkey = ps_partkey and 
               p_retailprice between 2147483647.85 and -2147483647.85 and 
-              ps_supplycost between 10 and 564541'''
+              ps_supplycost between 10 and 564541;'''
 #Checking Projection, Aggregation with constraints over attribute.
 Q7 = '''SELECT c_name, avg(c_acctbal) as max_balance, o_clerk 
         FROM customer, orders 
