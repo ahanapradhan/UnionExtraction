72927caf3553f1930bd596075152868d970281f8
Ahana <sendatahana@gmail.com>
2024-06-20 12:11:19 +0530

    Decimal

--- mysite/unmasque/src/core/abstract/un2_where_clause.py
+++ mysite/unmasque/src/core/abstract/un2_where_clause.py
@@ -75,7 +75,7 @@ class UN2WhereClause(MutationPipeLineBase):
             return 'date'
         elif any(x in self.attrib_types_dict[tab_attrib] for x in ['text', 'char', 'varbit', 'varchar2','varchar']):
             return 'str'
-        elif any(x in self.attrib_types_dict[tab_attrib] for x in ['numeric', 'float', 'decimal']):
+        elif any(x in self.attrib_types_dict[tab_attrib] for x in ['numeric', 'float', 'decimal', 'Decimal']):
             return 'numeric'
         else:
             raise ValueError
