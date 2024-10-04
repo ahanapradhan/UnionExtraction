c5ac01d2b8c058d4c1d3837b266f1814e8be4f27
Ahana <sendatahana@gmail.com>
2024-10-04 07:11:46 +0530

    plots

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -20,17 +20,15 @@ def signal_handler(signum, frame):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    hq = "(SELECT     c_custkey as order_id,     COUNT(*) AS total FROM     customer, orders where c_custkey = " \
-         "o_custkey and     o_orderdate >= '1995-01-01' GROUP BY     c_custkey ORDER BY     total ASC LIMIT 10) UNION " \
-         "ALL (SELECT     l_orderkey as order_id,     AVG(l_quantity) AS total FROM     orders, lineitem where " \
-         "l_orderkey = o_orderkey     AND o_orderdate < DATE '1996-07-01' GROUP BY     l_orderkey ORDER BY     total " \
-         "DESC LIMIT 10);"
+    hq = """SELECT o_custkey as key, sum(c_acctbal), o_clerk, c_name from orders FULL OUTER JOIN customer on c_custkey = o_custkey and
+o_orderstatus = 'F' group by o_custkey, o_clerk, c_name order by key
+limit 35;"""
 
     conn = ConnectionHelperFactory().createConnectionHelper()
-    conn.config.detect_union = True
-    conn.config.detect_oj = False
+    conn.config.detect_union = False
+    conn.config.detect_oj = True
     conn.config.detect_nep = False
-    conn.config.use_cs2 = False
+    conn.config.use_cs2 = True
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
     factory = PipeLineFactory()
