1144b8234f373f4761791a68c38fa155f1ef5475
Ahana <sendatahana@gmail.com>
2024-10-05 11:42:13 +0530

    expt query

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -20,15 +20,13 @@ def signal_handler(signum, frame):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    hq = """(SELECT c_custkey, c_name FROM customer,  nation where c_nationkey = n_nationkey and n_name = 'UNITED STATES' Order By c_custkey desc Limit 5) 
- UNION ALL (SELECT s_suppkey, s_name FROM supplier ,  nation where s_nationkey = n_nationkey and n_name = 'CANADA' Order By s_suppkey Limit 6) 
- UNION ALL (SELECT p_partkey, p_name FROM part ,  lineitem where p_partkey = l_partkey and l_quantity > 20 Order By p_partkey desc Limit 7) 
- UNION ALL (SELECT ps_partkey, p_name FROM part ,  partsupp where p_partkey = ps_partkey and ps_supplycost >= 1000 Order By ps_partkey Limit 8);"""
-
+    hq = "select n_name, c_acctbal from nation LEFT OUTER JOIN customer " \
+                "ON n_nationkey = c_nationkey and c_nationkey > 3 and " \
+                "n_nationkey < 20 and c_nationkey != 10 and c_acctbal < 7000 LIMIT 200;"
     conn = ConnectionHelperFactory().createConnectionHelper()
-    conn.config.detect_union = True
-    conn.config.detect_oj = False
-    conn.config.detect_nep = False
+    conn.config.detect_union = False
+    conn.config.detect_oj = True
+    conn.config.detect_nep = True
     conn.config.use_cs2 = False
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
--- mysite/unmasque/test/experiments/extraction/misc.sql
+++ mysite/unmasque/test/experiments/extraction/misc.sql
@@ -11,3 +11,17 @@ and s_nationkey = n_nationkey and n_name = 'ARGENTINA' and
 o_orderdate between '1998-01-01' and '1998-01-05' and o_totalprice >
 s_acctbal and o_totalprice >= 30000 and 50000 >= s_acctbal Order by account_balance desc limit 20);
 --QE
+
+
+--MQ7
+--QH
+select n_name, c_acctbal from nation LEFT OUTER JOIN customer ON n_nationkey = c_nationkey and c_nationkey > 3 and n_nationkey < 20 and c_nationkey != 10 and c_acctbal < 7000 LIMIT 200;
+--QE
+Select n_name, c_acctbal
+ From  customer
+ RIGHT OUTER JOIN  nation
+         ON customer.c_nationkey = nation.n_nationkey
+         and customer.c_nationkey between 4 and 19
+         and customer.c_acctbal <= 6999.99
+         and customer.c_nationkey <> 10
+ Limit 200;
