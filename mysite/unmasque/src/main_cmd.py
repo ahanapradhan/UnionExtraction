4aa0c2a4997b0d8b602c22ef3a5a9d90f5d6427e
Ahana <sendatahana@gmail.com>
2024-10-16 19:54:11 +0530

    expt more

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -20,13 +20,13 @@ def signal_handler(signum, frame):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    hq = "select n_name, c_acctbal from nation LEFT OUTER JOIN customer " \
-                "ON n_nationkey = c_nationkey and c_nationkey > 3 and " \
-                "n_nationkey < 20 and c_nationkey != 10 and c_acctbal < 7000 LIMIT 200;"
+    hq = """(SELECT o_orderkey, o_orderdate, n_name FROM orders, customer, nation where o_custkey = c_custkey and c_nationkey = n_nationkey and c_name like '%0001248%'  AND o_orderdate >= '1997-01-01' order by o_orderkey Limit 20) 
+ UNION ALL (SELECT l_orderkey, l_shipdate, o_orderstatus FROM lineitem, orders where l_orderkey = o_orderkey and o_orderdate < '1994-01-01'   AND l_quantity > 20   AND l_extendedprice > 1000 order by l_orderkey Limit 5);"""
+
     conn = ConnectionHelperFactory().createConnectionHelper()
-    conn.config.detect_union = False
-    conn.config.detect_oj = True
-    conn.config.detect_nep = True
+    conn.config.detect_union = True
+    conn.config.detect_oj = False
+    conn.config.detect_nep = False
     conn.config.use_cs2 = False
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
