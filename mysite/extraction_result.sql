df10a7b02d6a52c8090013572656e98f6c9e1df2
Ahana <sendatahana@gmail.com>
2024-12-04 15:05:12 +0530

    cs2 bugfix
    (cherry picked from commit d7be91b425f87d24e159184b9ecf08bcd6f1f21e)

--- mysite/extraction_result.sql
+++ mysite/extraction_result.sql
@@ -621,3 +621,24 @@ c_acctbal > 30.04;
  Order By price asc, country asc, entity_name asc 
  Limit 10;
  --- END OF ONE EXTRACTION EXPERIMENT
+
+ --- START OF ONE EXTRACTION EXPERIMENT
+ --- input query:
+ (select l_partkey as key from lineitem, part where l_partkey = p_partkey and l_extendedprice <= 905) union all (select l_orderkey as key from lineitem, orders where l_orderkey = o_orderkey and o_totalprice <= 905) union all (select o_orderkey as key from customer, orders where c_custkey = o_custkey and o_totalprice <= 890);
+ --- extracted query:
+  
+ (Select o_orderkey as key 
+ From lineitem, orders 
+ Where lineitem.l_orderkey = orders.o_orderkey
+ and orders.o_totalprice <= 905.0)
+ UNION ALL  
+ (Select l_partkey as key 
+ From lineitem, part 
+ Where lineitem.l_partkey = part.p_partkey
+ and lineitem.l_extendedprice <= 905.0)
+ UNION ALL  
+ (Select o_orderkey as key 
+ From customer, orders 
+ Where customer.c_custkey = orders.o_custkey
+ and orders.o_totalprice <= 890.0);
+ --- END OF ONE EXTRACTION EXPERIMENT
