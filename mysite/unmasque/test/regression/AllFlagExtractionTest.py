815448a8b88314a10f909fec3c67c1c19e601859
Ahana <sendatahana@gmail.com>
2024-12-24 23:38:17 +0530

    Q7, Q8

--- mysite/unmasque/test/regression/AllFlagExtractionTest.py
+++ mysite/unmasque/test/regression/AllFlagExtractionTest.py
@@ -293,6 +293,52 @@ where
         self.conn.config.detect_or = True
         self.do_test(query)
 
+    def test_Q8(self):
+        self.conn.config.use_cs2 = False
+        self.conn.config.detect_oj = False
+        self.conn.config.detect_union = False
+        self.conn.config.detect_nep = False
+
+        query = """select
+        o_year,
+        sum(case
+                when nation = 'INDIA' then volume
+                else 0
+        end) / sum(volume) as mkt_share
+from
+        (
+                select
+                        extract(year from o_orderdate) as o_year,
+                        l_extendedprice * (1 - l_discount) as volume,
+                        n2.n_name as nation
+                from
+                        part,
+                        supplier,
+                        lineitem,
+                        orders,
+                        customer,
+                        nation n1,
+                        nation n2,
+                        region
+                where
+                        p_partkey = l_partkey
+                        and s_suppkey = l_suppkey
+                        and l_orderkey = o_orderkey
+                        and o_custkey = c_custkey
+                        and c_nationkey = n1.n_nationkey
+                        and n1.n_regionkey = r_regionkey
+                        and r_name = 'ASIA'
+                        and s_nationkey = n2.n_nationkey
+                        and o_orderdate between date '1995-01-01' and date '1996-12-31'
+                        and p_type = 'ECONOMY ANODIZED STEEL'
+        ) as all_nations
+group by
+        o_year
+order by
+        o_year;"""
+        self.conn.config.detect_or = False
+        self.do_test(query)
+
     def test_unionQ(self):
         query = "(select l_partkey as key from lineitem, part " \
                 "where l_partkey = p_partkey and l_extendedprice <= 905) " \
