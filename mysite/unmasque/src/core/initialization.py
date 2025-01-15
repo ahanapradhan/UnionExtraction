7a2855d708ecafb791f9873f85aaf7d17d4b656c
Gopinath Patel <gopinath4301@gmail.com>
2025-01-15 17:06:59 +0530

    pk-fk none value error resolved.

--- mysite/unmasque/src/core/initialization.py
+++ mysite/unmasque/src/core/initialization.py
@@ -61,6 +61,8 @@ class Initiator(Base):
         all_relations = []
         temp = []
         for row in all_pkfk:
+            if len(row) < 6:
+                continue
             all_relations.append(row[0])
             if row[2].upper() == 'Y':
                 self.global_pk_dict[row[0]] = self.global_pk_dict.get(row[0], '') + ("," if row[0] in temp else '') + \
