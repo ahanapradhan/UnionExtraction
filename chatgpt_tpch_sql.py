a4d7bffbf37ec5e6a50e62018a3062e768720b83
Ahana <sendatahana@gmail.com>
2025-02-05 10:05:36 +0530

    etpch expt continued

--- chatgpt_tpch_sql.py
+++ chatgpt_tpch_sql.py
@@ -1,18 +1,21 @@
 ```sql
-SELECT partsupp.ps_partkey, nation.n_name, SUM(partsupp.ps_availqty * partsupp.ps_supplycost) AS part_value
-FROM nation, supplier, partsupp
-WHERE supplier.s_nationkey = nation.n_nationkey 
-  AND supplier.s_suppkey = partsupp.ps_suppkey 
-  AND nation.n_name = 'INDIA'
-GROUP BY partsupp.ps_partkey, nation.n_name
-HAVING SUM(partsupp.ps_availqty * partsupp.ps_supplycost) >= 
-       (SELECT SUM(ps_availqty * ps_supplycost) * 0.00001 
-        FROM partsupp, supplier, nation 
-        WHERE supplier.s_suppkey = partsupp.ps_suppkey 
-          AND supplier.s_nationkey = nation.n_nationkey 
-          AND nation.n_name = 'INDIA')
-ORDER BY part_value DESC;
+SELECT SUM(revenue_increase) AS total_revenue_increase
+FROM (
+    SELECT wl_extendedprice * wl_discount AS revenue_increase
+    FROM web_lineitem
+    WHERE wl_shipdate >= DATE '1993-01-01'
+      AND wl_shipdate < DATE '1995-01-01'
+      AND wl_discount BETWEEN 0.05 AND 0.07
+      AND wl_quantity < 24
+    UNION ALL
+    SELECT sl_extendedprice * sl_discount AS revenue_increase
+    FROM store_lineitem
+    WHERE sl_shipdate >= DATE '1993-01-01'
+      AND sl_shipdate < DATE '1995-01-01'
+      AND sl_discount BETWEEN 0.05 AND 0.07
+      AND sl_quantity < 24
+) AS combined_lineitems;
 ```
 
-Token count = 2124
+Token count = 2067
 
