9be19381f98126ed2122b483959c2498a825cd55
Ahana <sendatahana@gmail.com>
2025-02-22 05:19:20 +0530

    ok

--- mysite/gpt/output/gpt-4o_Q13_chatgpt.sql
+++ mysite/gpt/output/gpt-4o_Q13_chatgpt.sql
@@ -57,3 +57,31 @@ ORDER BY
 
 -- Prompt Token count = 6145
 
+```sql
+SELECT 
+    subquery.c_count, 
+    COUNT(*) AS c_orderdate, 
+    subquery.custdist 
+FROM 
+    (
+        SELECT 
+            o.o_orderdate AS c_count, 
+            COUNT(o.o_orderkey) AS custdist 
+        FROM 
+            customer c 
+        LEFT JOIN 
+            orders o 
+        ON 
+            c.c_custkey = o.o_custkey 
+            AND o.o_comment NOT LIKE '%special%requests%' 
+        GROUP BY 
+            o.o_orderdate, c.c_custkey
+    ) AS subquery 
+GROUP BY 
+    subquery.c_count, subquery.custdist 
+ORDER BY 
+    subquery.c_count DESC, subquery.custdist DESC;
+```
+
+-- Prompt Token count = 6356
+
